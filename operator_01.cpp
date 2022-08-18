c4c166a41ee83f7e710b09039866cf2b7f021dbc
nazaroveg <nazaroveg@yandex.ru>
2022-08-18 12:32:32 +0300

    v3

--- operator_01.cpp
+++ operator_01.cpp
@@ -19,27 +19,195 @@ public:
 
 	bool operator == (const Fraction& other)
 	{
-		return other.numerator_ == other.denominator_;
+		if (denominator_ == other.denominator_ && numerator_ == other.numerator_)
+		{
+			return true;	
+		}
+		else return false;
 	}
 	bool operator != (const Fraction& other)
 	{
-		return other.numerator_ != other.denominator_;   // return !(other.numerator_ == other.denominator_);  либо, и т.д.
+		if (denominator_ != other.denominator_ || numerator_ != other.numerator_)
+		{
+			return true;
+		}
+		else return false;
 	}
-	bool operator < (const Fraction& other)
+	bool operator < (const Fraction& other) 
 	{
-		return other.numerator_ < other.denominator_;
+		if (denominator_ == other.denominator_)
+		{
+			if (numerator_ < other.numerator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		if (numerator_ == other.numerator_)
+		{
+			if (denominator_ > other.denominator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		if (numerator_ != other.numerator_ && denominator_ != other.denominator_)
+		{
+			
+			int temp = denominator_ * other.denominator_;
+			numerator_ = numerator_ * temp;
+			//denominator_ = denominator_ * temp;
+			int numerator_1 = other.numerator_ * temp;
+			//int denominator_1 = denominator_ * temp;
+			if (numerator_ < other.numerator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
 	bool operator > (const Fraction& other)
 	{
-		return other.numerator_ > other.denominator_;
+		if (denominator_ == other.denominator_)
+		{
+			if (numerator_ > other.numerator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		if (numerator_ == other.numerator_)
+		{
+			if (denominator_ < other.denominator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		if (numerator_ != other.numerator_ && denominator_ != other.denominator_)
+		{
+
+			int temp = denominator_ * other.denominator_;
+			numerator_ = numerator_ * temp;
+			//denominator_ = denominator_ * temp;
+			int numerator_1 = other.numerator_ * temp;
+			//int denominator_1 = denominator_ * temp;
+			if (numerator_ > other.numerator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+
+
+
+
 	}
-	bool operator <= (const Fraction& other)
+	bool operator <= (const Fraction& other) 
 	{
-		return other.numerator_ <= other.denominator_;
+		if (denominator_ == other.denominator_)
+		{
+			if (numerator_ >= other.numerator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		if (numerator_ == other.numerator_)
+		{
+			if (denominator_ <= other.denominator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		if (numerator_ != other.numerator_ || denominator_ != other.denominator_)
+		{
+
+			int temp = denominator_ * other.denominator_;
+			numerator_ = numerator_ * temp;
+			//denominator_ = denominator_ * temp;
+			int numerator_1 = other.numerator_ * temp;
+			//int denominator_1 = denominator_ * temp;
+			if (numerator_ < numerator_1)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+
+
 	}
 	bool operator >= (const Fraction& other)
 	{
-		return other.numerator_ >= other.denominator_;
+		if (denominator_ == other.denominator_)
+		{
+			if (numerator_ <= other.numerator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		if (numerator_ == other.numerator_)
+		{
+			if (denominator_ >= other.denominator_)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		if (numerator_ != other.numerator_ || denominator_ != other.denominator_)
+		{
+
+			int temp = denominator_ * other.denominator_;
+			numerator_ = numerator_ * temp;
+			//denominator_ = denominator_ * temp;
+			int numerator_1 = other.numerator_ * temp;
+			//int denominator_1 = denominator_ * temp;
+			if (numerator_ > numerator_1)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
 	}
 
 };
